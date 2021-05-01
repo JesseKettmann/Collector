37ba8f517b946ef1326f9c4cdbaea57e3f6b38ab
Jesse Kettmann <jessekettmann@gmail.com>
2021-05-01 23:00:10 +0200

    added Mongo and Cron

--- bot.js
+++ bot.js
@@ -6,19 +6,39 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const botID = "838126284843778089";
 
+// MongoDB
+const mongoose = require("mongoose");
+const MongoClient = require('mongodb').MongoClient;
+
+// Cronjob
+const CronJob = require('cron').CronJob;
+
+// Settings
+const prefix = ".c";
+
 // On startup
 client.on("ready", () => {
     // Log
     console.log("Bot succesfully started");
 
     // Connect to MongoDB
-    //MongoDB.Connect();
+    mongoose.connect(process.env.BOT_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    }).then(() => {
+        console.log("Connected to MongoDB");
+        //OnStartup();
+    }).catch((err) => {
+        console.log("Failed to connect to MongoDB")
+        console.log(err);
+    });
 
     // Stay active
-    //var job = new CronJob('* 20 * * * *', function () {
-    //    console.log('Ping');
-    //}, null, true, 'America/Los_Angeles');
-    //job.start();
+    var job = new CronJob('* 20 * * * *', function () {
+        console.log('Keepalive Ping');
+    }, null, true, 'America/Los_Angeles');
+    job.start();
 });
 
 // On message
